f19836498251ab1301036529e0ce9880bc22aa3a
LZY <513523798@qq.com>
2024-06-13 11:32:00 +0800

    v4

--- demo/account/views.py
+++ demo/account/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 def login(request):
@@ -8,3 +9,16 @@ def login(request):
         password = request.POST.get('password')
         return HttpResponse(f"用户{username}的密码是{password}")
     return render(request, 'login.html')
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
+    
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        if password == password2:
+            return HttpResponse(f"用户{username}的密码是{password}")
+        else:
+            return HttpResponse("密码不匹配")
